f52991432474ed0350e2ed6a114963811807b056
BennyG117 <bengomezp@gmail.com>
2023-05-16 20:31:28 -0700

    Revised Making Connections Practice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("page loaded...");
+
+
+var placeholder1 = document.querySelector(".clickRemove1");
+
+function removeMe1() {
+    placeholder1.remove();
+}
+
+
+var placeholder2 = document.querySelector(".clickRemove2");
+
+function removeMe2() {
+    placeholder2.remove();
+}
+
+var profileName = document.querySelector(".card-body h1")
+
+function switchMe() {
+    profileName.innerText = "John Smith"
+}
+    
+
